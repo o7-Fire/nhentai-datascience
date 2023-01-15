67b9d603d918b8567b22466f87e4fc267b4d8d88
itzbenz <49940811+Itzbenz@users.noreply.github.com>
2023-01-15 04:46:18 +0100

    fatal flaw

--- idownloadedentirenhentaicdn.py
+++ idownloadedentirenhentaicdn.py
@@ -35,15 +35,16 @@ def media_id_page_to_file(media_id, page):
     return parsed_url.hostname + parsed_url.path.replace("/", "_").strip()
 
 
-def http_get(media_id, page, filename, session=None):
+def http_get(media_id, page, filename=None, session=None):
     if session is None:
         session = requests.Session()
+    if filename is None:
+        filename =  DATA_DIR+media_id_page_to_file(media_id, page)
     ext = filtered_id_ext[media_id]
     url = "https://i3.nhentai.net/galleries/" + str(media_id) + "/" + str(page) + ext
     parsed_url = urlparse(url)
     # print(url)
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
+    with session.get(url, stream=True) as r:
         if r.status_code != 200:
             return None
         with open(filename, 'wb') as f:
@@ -53,7 +54,7 @@ def http_get(media_id, page, filename, session=None):
     return parsed_url.hostname + parsed_url.path.replace("/", "_"), filename
 
 
-print(http_get(filtered_id[1], 1)[0])
+#print(http_get(filtered_id[1], 1)[0])
 
 alreadyHere = os.listdir(DATA_DIR)
 print("Already Here: " + str(len(alreadyHere)))
